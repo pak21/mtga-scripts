20bae2523022ca90fdc8e669c29dbc2c33dd8c75
Philip Kendall <philip@shadowmagic.org.uk>
2020-06-21 18:16:02 +0100

    Change to Selenium to automatically fetch deck title.

--- goldfish-fetcher.py
+++ goldfish-fetcher.py
@@ -7,9 +7,14 @@ import collections
 import contextlib
 import os
 import re
+
 from lxml import html
 import mysql.connector as mysql
 import requests
+import selenium.webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 EXPORT_URL_TEMPLATE = 'https://www.mtggoldfish.com/deck/arena_download/{}'
 
@@ -64,21 +69,24 @@ def main():
     parser.add_argument('-u', '--url', required=True)
     args = parser.parse_args()
 
-    page = requests.get(args.url)
-    tree = html.fromstring(page.content)
-
-    decks = tree.xpath('//ins[@class="widget-deck-placeholder"]')
+    driver = selenium.webdriver.Chrome()
+    driver.get(args.url)
 
     with contextlib.closing(conn.cursor()) as cursor:
-        for deck_content in decks:
-            cursor.execute('insert into decks set name = "Imported Deck", source = %s', (args.url,))
+        for deck_content in driver.find_elements_by_class_name('widget-deck-placeholder'):
+            wait = WebDriverWait(deck_content, 30)
+            title_element = wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'deck-view-title')))
+
+            deck_title = re.sub(r'(.*) by .*', r'\1', title_element.text)
+            print('Found deck {}'.format(deck_title))
+            cursor.execute('insert into decks set name = %s, source = %s', (deck_title, args.url))
 
             cursor.execute('select last_insert_id()')
             deck_id = cursor.fetchall()[0][0]
-            print('Importing deck {}'.format(deck_id))
+            print('Deck has ID {}'.format(deck_id))
 
-            export_url = EXPORT_URL_TEMPLATE.format(deck_content.get('data-id'))
-            print('Fetching deck from {}'.format(export_url))
+            export_url = EXPORT_URL_TEMPLATE.format(deck_content.get_attribute('data-id'))
+            print('Fetching deck contents from {}'.format(export_url))
             export_page = requests.get(export_url)
             export_tree = html.fromstring(export_page.content)
 
@@ -94,6 +102,8 @@ def main():
                 cursor.execute('insert into deck_cards set deck_id = %s, name = %s, main = %s, sideboard = %s, is_companion = %s', (deck_id, name, *counts, is_companion))
 
         conn.commit()
+
+    driver.quit()
 
 if __name__ == '__main__':
     main()
