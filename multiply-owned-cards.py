3a2dd1186e6b4149b834b0d8b4d150f8ba09e70c
Philip Kendall <philip@shadowmagic.org.uk>
2020-07-25 19:13:05 +0100

    Move multiply-owned-cards to use mtga module.

--- multiply-owned-cards.py
+++ multiply-owned-cards.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
-import contextlib
-import os
-
-import mysql.connector as mysql
 import pandas as pd
+
+import mtga
 
 CARDS_SQL="select cards.name, cards.rarity, sum(collection.count) as n from collection join cards on collection.mtga_id = cards.mtga_id where cards.rarity != 'Basic' group by cards.name having n > 4 order by n desc, cards.name"
 
+def get_data(cursor):
+    cursor.execute(CARDS_SQL)
+    return cursor.fetchall()
+
 def main():
-    conn = mysql.connect(database='mtga', user='philip', password=os.environ['DATABASE_PASSWORD'])
-    with contextlib.closing(conn.cursor()) as cursor:
-        cursor.execute(CARDS_SQL)
-        cards = pd.DataFrame(cursor.fetchall(), columns=['name', 'rarity', 'count'])
-        print(cards)
+    conn = mtga.connect()
+    cards = pd.DataFrame(mtga.with_cursor(conn, get_data), columns=['name', 'rarity', 'count'])
+    print(cards)
 
 if __name__ == '__main__':
     main()
